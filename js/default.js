8cc66e371b97ee51d1e391201e872d4e51b39ff5
胡文杰 <42986356+Monkey-Huwj@users.noreply.github.com>
2021-06-25 21:44:29 +0800

    提交文件

---
+++ js/default.js
@@ -0,0 +1,81 @@
+var indexdefault = {
+    pageLoad: function () {
+        var nowDate = new Date;
+        
+        $(".htmleaf-header h2").text(nowDate.Format("HH:mm:ss"));
+        $(".tab_right h3").text("左侧为当前体力最大值");
+        $(".tab_right h4").text("到达该时间体力将存满");
+        $(".tab_right p").text("超过左侧体力值目标时间将会溢出");
+        
+        indexdefault.setTimeLevel(indexdefault.getBeginDate());
+        indexdefault.setEnergyLevel(indexdefault.getBeginDate());
+
+        setInterval(function(){
+            indexdefault.pageLoad();
+        },1000);
+    },
+    getBeginDate:function(){
+        var nowDate = new Date;
+        var nowDate_y = nowDate.getFullYear();
+        var nowDate_M = nowDate.getMonth() + 1;
+        var nowDate_d = nowDate.getDate();
+        var nowDate_h = nowDate.getHours();
+        var nowDate_m = nowDate.getMinutes();
+        var nowDate_s = nowDate.getSeconds();
+        var beginDate = nowDate;
+        if(nowDate_m>=30){
+            var datestr=nowDate_y+"-"+nowDate_M+"-"+nowDate_d+" "+(nowDate_h+1)+":00:00";
+            beginDate=new Date(datestr.replace(/-/g,"/"));
+        }else{
+            var datestr=nowDate_y+"-"+nowDate_M+"-"+nowDate_d+" "+nowDate_h+":"+30+":00";
+            beginDate=new Date(datestr.replace(/-/g,"/"));
+        }
+        return beginDate;
+    },
+    setTimeLevel:function(beginDate){
+        beginDate.setSeconds(beginDate.getSeconds()-1800);
+        $(".app_inner__tab h2").each(function(){
+            beginDate.setSeconds(beginDate.getSeconds()+1800);
+            $(this).text(beginDate.Format("HH:mm"));
+        });
+
+        var maxDate = new Date;
+        maxDate.setSeconds(maxDate.getSeconds()+9990);
+        
+        $(".app_inner__tab h2")[5].innerHTML = maxDate.Format("HH:mm");
+    },
+    setEnergyLevel:function(beginDate){
+        var nowDate = new Date;
+        beginDate.setSeconds(beginDate.getSeconds()-1800);
+        $(".tab_left__image h1").each(function(){
+            beginDate.setSeconds(beginDate.getSeconds()+1800);
+            var energy = indexdefault.getEnergyByTimeDiff(nowDate,beginDate)
+            $(this).text(energy);
+        });
+        $(".tab_left__image h1")[5].innerHTML =0;
+    },
+    getEnergyByTimeDiff: function (nowDate,beginDate) {
+        var maxEnergySeconds = 999*10*1000;
+        var diffSeconds = beginDate - nowDate;
+        var energy = (maxEnergySeconds - diffSeconds) / 1000 / 10
+        return Math.ceil(energy);;
+    }
+};
+
+Date.prototype.Format = function (fmt) { //author: meizz
+    var o = {
+    "M+": this.getMonth() + 1, //月份
+    "d+": this.getDate(), //日
+    "H+": this.getHours(), //小时
+    "m+": this.getMinutes(), //分
+    "s+": this.getSeconds(), //秒
+    "q+": Math.floor((this.getMonth() + 3) / 3), //季度
+    "S": this.getMilliseconds() //毫秒
+    };
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+    if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+    }
+
+$(document).ready(indexdefault.pageLoad);
